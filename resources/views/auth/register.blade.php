9a3968a58e439442c39733473b3a308e6aea6e83
Jamie Jansen <jamie@jnsn.me>
2021-07-12 20:12:51 -0700

    Add terms of service

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -60,6 +60,29 @@
                 </div>
 
                 <div class="field">
+                    <h2 class="label">Terms of Service</h2>
+                    <div class="tos textarea">
+                        <p>I agree to rate movies according to their Leftism as determined with an objective measurement
+                            using the proprietary solution as outlined in the audio clip located at
+                            <a href="https://globehell.libsyn.com/episode-53-lefterboxd" rel="noopener" target="new">https://globehell.libsyn.com/episode-53-lefterboxd</a>.</p>
+
+                        <p>By creating this account, I certify that I have listened to and comprehensively understood the crucial details and methodology
+                            outlined in the above linked audio clip. All movies for which I submit a vote have been personally viewed using some apparatus
+                            to obfuscate the rightmost third of the display device used in the viewing.</p>
+
+                        <p>TODO: elaborate on the specifics of the terms of service</p>
+
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam odio nisi, tincidunt ut turpis sit amet, suscipit elementum sem. Maecenas lobortis augue eu dui iaculis gravida. Nullam pharetra nisl quis sem viverra pretium. Cras nec tellus tincidunt, euismod lorem nec, feugiat arcu. In pretium nunc metus, a interdum ligula gravida quis. Sed varius at tortor in laoreet. Nulla sagittis est et varius iaculis.</p>
+                    </div>
+
+                    <label class="checkbox" for="tos">
+                        <input type="checkbox" name="tos" id="tos" {{ old('tos') ? 'checked' : '' }}>
+                        {{ __('I agree') }}
+                    </label>
+                </div>
+
+                <div class="field">
                     <button type="submit" class="button is-primary">
                         {{ __('Sign up') }}
                     </button>

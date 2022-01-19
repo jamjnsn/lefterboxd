d6a2110a5f602020ec98b3d28ee59edf2a63a73e
Jamie Jansen <jamie@jnsn.me>
2022-01-19 13:36:39 -0800

    Add support for overriding base URL

--- config/app.php
+++ config/app.php
@@ -53,6 +53,7 @@ return [
     */
 
     'url' => env('APP_URL', 'http://localhost'),
+    'base_url' => env('BASE_URL', null),
 
     'asset_url' => env('ASSET_URL', null),
 
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Config;
+
+$baseUrl = Config::get('app.base_url');
+
+if($baseUrl) {
+    URL::forceRootUrl($baseUrl);
+}
 
 Route::middleware('auth')->group(function() {
     Route::get('/movie/{id}/vote', [App\Http\Controllers\VoteController::class, 'get']);
